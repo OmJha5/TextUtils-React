fb154c83eac14efb137a8e1989d1c48fd8935fa5
om <omjha003@gmail.com>
2024-11-05 19:52:27 +0530

    Added buttons to add logic of converting text to uppercase , then added buttons to copy and paste from clipboard

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,13 +11,43 @@ export default function TextForm(props) {
         setText(event.target.value)
     }
 
+    function onClickToClear() {
+        setText("");
+    }
+
+    function onClickCopy(){
+        navigator.clipboard.writeText(text).then(() => {
+            console.log("Copy to CLipboard");
+        })
+        .catch(() => {
+            console.log("Error Is coming while coping")
+        })
+    }
+
+    function onClickPaste(){
+        navigator.clipboard.readText().then((clipboardText) => {
+            setText(clipboardText);
+        })
+        .catch(() => {
+            console.log("Error Is coming while Pasting")
+        })
+    }
+
   return (
     <>
 
         <div className="mb-3 mt-5">
             <h1>{props.heading}</h1>
             <textarea className="form-control" id="exampleFormControlTextarea1" rows="6" value={text} onChange={onChangeText}></textarea>
-            <button className="btn btn-primary" onClick={onClickToUC}>Convert To UpperCase</button>
+            <br />
+            <button className="btn btn-primary" onClick={onClickToUC}>Convert To UpperCase</button> &nbsp;
+            <button className="btn btn-primary" onClick={onClickToClear}>Clear</button> &nbsp;
+            <button className="btn btn-primary" onClick={onClickCopy}>Copy To ClipBoard</button> &nbsp;
+            <button className="btn btn-primary" onClick={onClickPaste}>Paste</button>
+        </div>
+
+        <div className="mt-3">
+            <p>Your text has {(text == "") ? 0 : text.split(" ").length} words and {text.length} characters</p>
         </div>
 
     </>
